9cfcc21f9f6144bf973514b4a56f3e63d4611a65
congmeiba <hxs_82314976@126.com>
2018-11-28 18:32:44 +0800

    服务于注册的初步学习

---
+++ chapter1/eureka-server/src/main/java/cn/huangxs/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package cn.huangxs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+//@EnableEurekaServer 启动EurekaServer服务,把这个项目当成一个服务器
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
